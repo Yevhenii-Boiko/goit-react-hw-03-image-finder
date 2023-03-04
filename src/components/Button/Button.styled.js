9e47c7575d59048818eda98b2b026af842896227
Yevhenii <boiko.yevgenii@gmail.com>
2023-03-04 23:27:19 +0200

    adds loadmMoreBtn position

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 
 export const LoadMoreButton = styled.button`
   padding: 8px 16px;
+  width: 200px;
+  margin-left: auto;
+  margin-right: auto;
   border-radius: 2px;
   background-color: #3f51b5;
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
